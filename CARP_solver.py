975011673de6691495125964201682052820f8c5
ChuangYANG <31944157+SUSTC-ChuangYANG@users.noreply.github.com>
2018-11-22 04:59:11 +0800

    Update CARP_solver.py

--- CARP_solver.py
+++ CARP_solver.py
@@ -9,13 +9,8 @@ if __name__ == "__main__":
     time_limit = int(sys.argv[3])
     seed = int(sys.argv[5])
     start = time.time()
-    # file_name = "val1A.dat"
-    # time_limit = 120
     graph = file_to_graph(file_name)
     get_dijkstra_matrix(graph)
     idx = np.where((graph['demand_matrix'] != -1) & (graph['demand_matrix'] != 0))
     tasks = list(zip(idx[0], idx[1]))  # all direction are stored
-
-    MAENS(start=start,graph=graph,tasks=tasks,time_limit=time_limit,mul_process=True)
-    # plan = path_scanning(graph=graph, tasks=tasks, rule_code=1)
-    # print_result(plan, graph)
+    MAENS(start=start,graph=graph,tasks=tasks,time_limit=time_limit,mul_process=True) # mulprocess is False if you don't want to use
